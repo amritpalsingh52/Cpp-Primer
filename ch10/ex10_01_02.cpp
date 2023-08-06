330f1b7f5b8d13e75c0b0417abf7e2357b693244
amritpalsingh52 <124516230+amritpalsingh52@users.noreply.github.com>
2023-08-06 21:01:19 +0530

    Update ex10_01_02.cpp

--- ch10/ex10_01_02.cpp
+++ ch10/ex10_01_02.cpp
@@ -19,17 +19,17 @@
 #include <vector>
 #include <algorithm>
 #include <list>
-
+using namespace std;
 
 int main()
 {
     // 10.1
     std::vector<int> v = { 1, 2, 3, 4, 5, 6, 6, 6, 2 };
-    std::cout << "ex 10.01: " << std::count(v.cbegin(), v.cend(), 6) << std::endl;
+    std::cout << "ex 10.01: " << count(v.cbegin(), v.cend(), 6) << std::endl;
 
     // 10.2
     std::list<std::string> l = { "aa", "aaa", "aa", "cc" };
-    std::cout << "ex 10.02: " << std::count(l.cbegin(), l.cend(), "aa") << std::endl;
+    std::cout << "ex 10.02: " << count(l.cbegin(), l.cend(), "aa") << std::endl;
 
     return 0;
 }
